b2f7a8a28441124b0732ea1185c563725cc75a3e
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-03-15 20:45:16 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+BASEDIR=$(readlink -f $(dirname "$0") )
+ENV_FILE=${BASEDIR}/env
+
+[ -f ${ENV_FILE} ] || {
+    echo "Configuration file missing: $ENV_FILE"
+    exit 1
+}
+
+. $ENV_FILE
+
+docker run -d \
+    --name mysql \
+    -e MYSQL_ROOT_PASSWORD \
+    -e MYSQL_DATABASE \
+    -e MYSQL_USER \
+    -e MYSQL_PASSWORD \
+    -v /var/lib/mysql:/var/lib/mysql \
+    -p 3306:3306 \
+    -d mysql:5.7.25 \
+    --net_buffer_length=10485760 \
+    --max_allowed_packet=10485760
